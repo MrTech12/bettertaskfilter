a2ff04d85485c1cb3c47ada754717aae6bff9a72
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-02-06 13:41:54 +0100

    Changed: position of code
    * Changed which code pieces start on a new line.
    * Wrapped all main logic in the 'app.js' file in a function.

--- src/app.ts
+++ src/app.ts
@@ -4,25 +4,29 @@ const FilterHelper = require('./Helpers/FilterHelper');
 const DiscordHelper = require('./helpers/DiscordHelper');
 import logger = require('npmlog');
 
-if (process.env.TODOIST_TOKEN === "" || process.env.DISCORD_TOKEN === "" || process.env.TODOIST_TOKEN === undefined || process.env.DISCORD_TOKEN === undefined) {
-    logger.error("From Setup","The tokens for Todoist & Discord are not available.");
-    process.exit(1);
-}
-
-logger.info("From Setup","The tokens are available. The application is starting...");
-TodoistProvider.retrieveFilters().then((filters: FilterInterface[]) => {
-    let bucketFilter = filters.find(filter => filter.id == process.env.FILTER_BUCKET_ID);
-
-    TodoistProvider.retrieveTasks(bucketFilter).then((tasks: TaskInterface[]) => {
-        TodoistProvider.retrieveProjectNames(tasks).then((retrievedProjectNames:string[]) => {
-            let projectNames: string[] = [];
-            projectNames = [...retrievedProjectNames];
-
-            let newFilterQuery = FilterHelper.createFilterQuery(projectNames);
-
-            TodoistProvider.updateOrderFilter(newFilterQuery).then(() => {
-                DiscordHelper.sendStatusMessage();
+function updateFilter (): void {
+    if (process.env.TODOIST_TOKEN === "" || process.env.DISCORD_TOKEN === "" || process.env.TODOIST_TOKEN === undefined || process.env.DISCORD_TOKEN === undefined) {
+        logger.error("From Setup","The tokens for Todoist & Discord are not available.");
+        process.exit(1);
+    }
+    
+    logger.info("From Setup","The tokens are available. The application is starting...");
+    TodoistProvider.retrieveFilters().then((filters: FilterInterface[]) => {
+        let bucketFilter = filters.find(filter => filter.id == process.env.FILTER_BUCKET_ID);
+    
+        TodoistProvider.retrieveTasks(bucketFilter).then((tasks: TaskInterface[]) => {
+            TodoistProvider.retrieveProjectNames(tasks).then((retrievedProjectNames:string[]) => {
+                let projectNames: string[] = [];
+                projectNames = [...retrievedProjectNames];
+    
+                let newFilterQuery = FilterHelper.createFilterQuery(projectNames);
+    
+                TodoistProvider.updateOrderFilter(newFilterQuery).then(() => {
+                    DiscordHelper.sendStatusMessage();
+                });
             });
         });
     });
-});
+}
+
+updateFilter();
