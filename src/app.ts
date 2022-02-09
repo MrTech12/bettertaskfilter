e831698bb2a0382054b18dcbaef0361e10ec62a7
Sina Nazar Soroush <13730935+MrTech12@users.noreply.github.com>
2022-02-09 01:36:09 +0100

    Change: calling other logic after 'then' in app.ts
    * Changing how other logic is called in the 'app.ts' file, after a
    Promise is resolved from the 'TodoistProvider'.
    * In tthe past, logic is added, after the '.then' method.
    * This previous approach let to callback hell and unorganized code.
    * The new method passed data from the Promise, to different methods
    with 'Promise chaining'
    * This provides cleaner and better code to understand.

--- src/app.ts
+++ src/app.ts
@@ -5,33 +5,48 @@ import * as DiscordHelper from './helpers/DiscordHelper';
 import * as DateTimeHelper from './helpers/DateTimeHelper';
 import logger from 'npmlog';
 
-function updateFilter (): void {
+async function InitiateApp (): Promise<void> {
     if (process.env.TODOIST_TOKEN === '' || process.env.DISCORD_TOKEN === '' || process.env.TODOIST_TOKEN === undefined || process.env.DISCORD_TOKEN === undefined) {
         logger.error(`From Setup @ ${DateTimeHelper.getDutchDateTime('short')}`,'The tokens for Todoist & Discord are not available.');
         process.exit(1);
     }
     
     logger.info(`From Setup @ ${DateTimeHelper.getDutchDateTime('short')}`,'The tokens are available. The application is starting...');
-    TodoistProvider.retrieveFilters().then((filters: FilterInterface[]) => {
-        let bucketFilter = filters.find(filter => filter.id == process.env.FILTER_BUCKET_ID);
-
-        if(bucketFilter == undefined) {
-            logger.error(`From Setup @ ${DateTimeHelper.getDutchDateTime('short')}`,'The bucket filter has not been saved properly.');
-            process.exit(1);
-        }
-        TodoistProvider.retrieveTasks(bucketFilter).then((tasks: TaskInterface[]) => {
-            TodoistProvider.retrieveProjectNames(tasks).then((retrievedProjectNames:string[]) => {
-                let projectNames: string[] = [];
-                projectNames = [...retrievedProjectNames];
-    
-                let newFilterQuery = FilterHelper.createFilterQuery(projectNames);
-    
-                TodoistProvider.updateOrderFilter(newFilterQuery).then(() => {
-                    DiscordHelper.sendStatusMessage();
-                });
-            });
-        });
-    });
+    await TodoistProvider.retrieveFilters()
+        .then((filterResult: FilterInterface[]) => getBucketFilter(filterResult))
+        .then((bucketFilter: FilterInterface) => getTasks(bucketFilter))
+        .then((tasks: TaskInterface[]) => getFilterQuery(tasks))
+        .then((newFilterQuery: string) => updateFilter(newFilterQuery));
 }
 
-updateFilter();
+function getBucketFilter(filters: FilterInterface[]): FilterInterface {
+    let bucketFilter = filters.find(filter => filter.id == process.env.FILTER_BUCKET_ID);
+
+    if(bucketFilter == undefined) {
+        logger.error(`From Setup @ ${DateTimeHelper.getDutchDateTime('short')}`,'The bucket filter has not been retrieved properly.');
+        process.exit(1);
+    }
+    return bucketFilter;
+}
+
+async function getTasks(bucketFilter: FilterInterface): Promise<TaskInterface[]> {
+    return await TodoistProvider.retrieveTasks(bucketFilter);
+}
+
+async function getFilterQuery(tasks: TaskInterface[]): Promise<string> {
+    let filterquery: string = '';
+
+    await TodoistProvider.retrieveProjectNames(tasks).then((retrievedProjectNames:string[]) => { 
+        let projectNames: string[] = [];
+        projectNames = [...retrievedProjectNames];
+
+        filterquery = FilterHelper.createFilterQuery(projectNames);
+    });
+    return filterquery;
+}
+
+async function updateFilter(newFilterQuery: string): Promise<void> {
+    await TodoistProvider.updateOrderFilter(newFilterQuery).then(async () => DiscordHelper.sendStatusMessage());
+}
+
+InitiateApp();
